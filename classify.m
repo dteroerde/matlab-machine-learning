b621d0bf7eb485976a3948f5d124792be8d87b66
dteroerde <dominik.teroerde@rub.de>
2021-01-17 17:24:36 +0100

    Clean code

--- classify.m
+++ classify.m
@@ -2,6 +2,24 @@ clc
 clear all;
 close all;
 
+% NEURAL NETWORK
+load('trainingsdaten_nn.mat');
+
+rng('default');
+hiddenLayerSize = [4 5 11];
+net = patternnet(hiddenLayerSize);
+
+net.divideParam.trainRatio = 0.9;
+net.divideParam.valRatio = 0.1;
+net.divideParam.testRatio = 0.0;
+
+[net, tr] = train(net, trainingInputs, trainingTargets);
+
+load('testdaten_nn.mat');
+y = net(testInputs);
+performance = perform(net, testInputs, y);
+accu_nn = computeAccuracy(testTargets, y);
+
 % MAXIMUM-LIKELIHOOD, BAYES-CLASSIFIER
 load('trainingsdaten.mat');
 
@@ -22,28 +40,10 @@ k(2).name = 'Malfunction';
 k(2).theta = theta_defekt;
 k(2).prior = 1 - k(1).prior;
 
+testdata = [testInputs(2,:); testInputs(1,:)];
+
 % classify
 class_ml = classifyML(testdata, k);
 class_bayes = classifyBayes(testdata, k);
 accu_ml = computeAccuracy(testTargets, class_ml);
 accu_bayes = computeAccuracy(testTargets, class_bayes);
-
-% NEURAL NETWORK
-load('trainingsdaten_nn.mat');
-
-rng('default');
-hiddenLayerSize = [4 5 11];
-net = patternnet(hiddenLayerSize);
-
-net.divideParam.trainRatio = 0.9;
-net.divideParam.valRatio = 0.1;
-net.divideParam.testRatio = 0.0;
-
-[net, tr] = train(net, trainingInputs, trainingTargets);
-
-load('testdaten_nn.mat');
-y = net(testInputs);
-performance = perform(net, testInputs, y);
-accu_nn = computeAccuracy(testTargets, y);
-
-testdata = [testInputs(2,:); testInputs(1,:)];
